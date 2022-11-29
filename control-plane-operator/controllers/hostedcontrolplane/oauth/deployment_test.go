adfcb2c6f82711ba282bb56c546e9efc9efdf26c
Bruce Cong <brucecong@Bruces-MBP.lan>
2022-11-29 12:16:03 -0600

    add oauth deployment test

--- control-plane-operator/controllers/hostedcontrolplane/oauth/deployment_test.go
+++ control-plane-operator/controllers/hostedcontrolplane/oauth/deployment_test.go
@@ -1,12 +1,16 @@
 package oauth
 
 import (
+	"context"
+	"github.com/openshift/hypershift/api"
 	hyperv1 "github.com/openshift/hypershift/api/v1alpha1"
 	"github.com/openshift/hypershift/control-plane-operator/hostedclusterconfigoperator/controllers/resources/manifests"
 	"github.com/openshift/hypershift/support/config"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"testing"
 )
 
@@ -31,23 +35,31 @@ func TestReconcileOauthDeployment(t *testing.T) {
 	testCases := []struct {
 		cm               corev1.ConfigMap
 		deploymentConfig config.DeploymentConfig
+		params           OAuthServerParams
 	}{
 		// empty deployment config
 		{
 			cm: corev1.ConfigMap{
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-ocm-config",
+					Name:      "test-oauth-config",
 					Namespace: targetNamespace,
 				},
 				Data: map[string]string{"config.yaml": "test-data"},
 			},
 			deploymentConfig: config.DeploymentConfig{},
+			params: OAuthServerParams{
+				AvailabilityProberImage: "test-availability-image",
+				Socks5ProxyImage:        "test-socks-5-proxy-image",
+			},
 		},
 	}
 	for _, tc := range testCases {
+		ctx := context.Background()
+		fakeClient := fake.NewClientBuilder().WithScheme(api.Scheme).Build()
 		expectedTermGraceSeconds := oauthDeployment.Spec.Template.Spec.TerminationGracePeriodSeconds
 		expectedMinReadySeconds := oauthDeployment.Spec.MinReadySeconds
-		err := ReconcileDeployment(oauthDeployment, ownerRef, imageName, &tc.cm, tc.deploymentConfig)
+		err := ReconcileDeployment(ctx, fakeClient, oauthDeployment, ownerRef, &tc.cm, imageName, tc.deploymentConfig, tc.params.IdentityProviders(), tc.params.OauthConfigOverrides,
+			tc.params.AvailabilityProberImage, pointer.Int32(1234), tc.params.NamedCertificates(), tc.params.Socks5ProxyImage, tc.params.NoProxy)
 		assert.NoError(t, err)
 
 		assert.Equal(t, expectedTermGraceSeconds, oauthDeployment.Spec.Template.Spec.TerminationGracePeriodSeconds)
